0339172fb4415bd9f3baac8da3d0b270db5abb5b
Anatoli Makarevich <makaroni4@gmail.com>
2019-01-15 22:28:10 +0100

    Add tests for incrementing time

--- test/tracker_test.js
+++ test/tracker_test.js
@@ -1,6 +1,6 @@
 const tk = require('timekeeper');
 
-import { readData } from '../src/tracker';
+import { readData, incrementTime } from '../src/tracker';
 
 describe('readData', () => {
   beforeEach(() => {
@@ -25,6 +25,8 @@ describe('readData', () => {
                   "2019-01-13": 100.2
                 }
               });
+            },
+            set: (data, callback) => {
             }
           }
         }
@@ -52,6 +54,8 @@ describe('readData', () => {
               callback({
                 "youtube_time_tracker_data": null
               });
+            },
+            set: (data, callback) => {
             }
           }
         }
@@ -70,3 +74,78 @@ describe('readData', () => {
     });
   });
 });
+
+describe('incrementTime', () => {
+  beforeEach(() => {
+    tk.freeze("Sun Jan 13 2019 20:38:45 GMT+0100 (Central European Standard Time)");
+  });
+
+  afterEach(() => {
+    tk.reset();
+  });
+
+  describe('when chrome.storage has data', () => {
+    it('calls callback function with timer data', () => {
+      global.chrome = {
+        storage: {
+          local: {
+            get: (key, callback) => {
+              callback({
+                "youtube_time_tracker_data": {
+                  "2019": 100.2,
+                  "jan-2019": 100.2,
+                  "2-2019": 100.2,
+                  "2019-01-13": 100.2
+                }
+              });
+            },
+            set: (data, callback) => {
+            }
+          }
+        }
+      };
+
+      const callback = jest.fn();
+
+      incrementTime(5, callback);
+
+      expect(callback).toHaveBeenCalledWith({
+        "2019": 105.2,
+        "jan-2019": 105.2,
+        "2-2019": 105.2,
+        "2019-01-13": 105.2
+      });
+    });
+  });
+
+  describe('when chrome.storage has no data', () => {
+    it('sets todays and this month keys to 0', () => {
+      global.chrome = {
+        storage: {
+          local: {
+            get: (key, callback) => {
+              callback({
+                "youtube_time_tracker_data": null
+              });
+            },
+            set: (data, callback) => {
+            }
+          }
+        }
+      };
+
+      const callback = jest.fn();
+
+      incrementTime(5, callback);
+
+      expect(callback).toHaveBeenCalledWith({
+        "2019": 5,
+        "jan-2019": 5,
+        "2-2019": 5,
+        "2019-01-13": 5
+      });
+    });
+  });
+});
+
+
